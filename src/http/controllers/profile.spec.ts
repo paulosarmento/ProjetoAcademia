b446ae93d7906fb1934c9f5aef855be7a97d3dfa
Mac <mac@MacBook-Pro-de-Mac.local>
2023-08-05 10:45:14 -0300

    Teste E2E do perfil

--- src/http/controllers/profile.spec.ts
+++ src/http/controllers/profile.spec.ts
@@ -1,3 +1,37 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {});
+describe("Profile (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+  it("Should be able to get user profile", async () => {
+    await request(app.server).post("/users").send({
+      name: "john Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+    const authResponse = await request(app.server).post("/sessions").send({
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+
+    const { token } = authResponse.body;
+
+    const profileResponse = await request(app.server)
+      .get("/me")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(profileResponse.statusCode).toEqual(200);
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: "johndoe@gmail.com",
+      })
+    );
+  });
+});
