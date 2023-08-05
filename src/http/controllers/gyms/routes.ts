8d7d5d999bfa2dd6af6a4df3164a4551c10e90b9
Mac <mac@MacBook-Pro-de-Mac.local>
2023-08-05 11:35:09 -0300

    Outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,15 @@
 import { FastifyInstance } from "fastify";
+
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
-import { register } from "../users/register";
-import { authenticate } from "../users/authenticate";
-import { profile } from "../users/profile";
+import { search } from "./search";
+import { create } from "./create";
+import { nearby } from "./nearby";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+
+  app.post("/gyms", create);
 }
