77f9bc1edf65f0651f17677e59220ab8135873a6
Mac <mac@MacBook-Pro-de-Mac.local>
2023-08-06 12:35:34 -0300

    Autorização por cargos (RBAC)

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -20,10 +20,13 @@ export async function authenticate(
       email,
       password,
     });
-    const token = await reply.jwtSign({}, { sign: { sub: user.id } });
+    const token = await reply.jwtSign(
+      { role: user.role },
+      { sign: { sub: user.id } }
+    );
 
     const refreshToken = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
