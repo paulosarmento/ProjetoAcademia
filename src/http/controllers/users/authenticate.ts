d73122cea49a30b8bc896d1e30d6b8239b029950
Mac <mac@MacBook-Pro-de-Mac.local>
2023-08-06 09:24:52 -0300

    Estratégia de refresh de token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -21,7 +21,28 @@ export async function authenticate(
       password,
     });
     const token = await reply.jwtSign({}, { sign: { sub: user.id } });
-    return reply.status(200).send({ token });
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      }
+    );
+
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ error: err.message });
